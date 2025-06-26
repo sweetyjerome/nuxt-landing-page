e9b552ff1d6a5e1936d9646e0dfedc8daebd1828
Sweety_Elizabeth Jerome <jerome@fim.uni-passau.de>
2025-06-26 17:23:57 +0100

    layout -wip

--- app/components/Hero.vue
+++ app/components/Hero.vue
@@ -4,41 +4,25 @@
     <img src="/img/atlantean_treasures_logo.png" alt="Game Logo" class="game-logo" />
 
     <div class="hero-content">
-      <!-- Gameplay video -->
-      <div class="video">
-        <h1>200 Free Spins!</h1>
-        <p class="offer-sub">Exclusive offer just for you</p>
-        <video autoplay muted loop playsinline>
+      <!-- Left Section: Heading, Subheading, and Video -->
+      <div class="hero-left">
+        <div class="info-box">
+          <h5 class="info">Exclusive offer</h5>
+        </div>
+        <p class="hero-heading"> Grab 200 Free Spins</p>
+        <p class="hero-subheading">For just 10$ deposit</p>
+        <video autoplay muted loop playsinline class="hero-video">
           <source src="https://cdn.desdev.co/atlanteantreasures.webm" type="video/webm" />
           Your browser does not support the video tag.
         </video>
+        <button class="claim-now-btn">Claim Now</button>
       </div>
 
-      <!-- Main Content
-      <div class="content">
-        <div class="content-box">
-          <h1>200 Free Spins!</h1>
-          <p class="offer-sub">Exclusive offer just for you</p>
-
-          <div class="jackpot">
-            <p v-if="pending">Fetching Jackpot...</p>
-            <p v-else-if="error">Jackpot unavailable</p>
-            <p v-else>
-              Current Jackpot: <span class="amount">{{ formattedJackpot }}</span>
-            </p>
-          </div>
-
-          <button class="glow-button">Claim Now</button>
-          <p class="terms">Deposit just $10 and grab your riches</p>
-        </div>
-      </div>  -->
-
-
-      <!-- Right Graphics -->
-      <div class="decorations">
+      <!-- Right Section: Images -->
+      <div class="hero-right">
         <img src="/img/bubble_1.png" alt="Bubble-1" class="bubble" />
         <img src="/img/bubble_2.png" alt="Bubble-2" class="number" />
-      </div> 
+      </div>
     </div>
   </section>
 </template>
@@ -60,17 +44,17 @@ const formattedJackpot = computed(() =>
 .hero {
   position: relative;
   width: 100vw;
-  height: 100dvh;
+  padding: 2rem 6rem;
   background: url('/img/hero.jpg') no-repeat center center / cover;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: start;
+  justify-content: center;
   overflow: hidden;
-  padding-top: 2rem;
+  box-sizing: border-box;
 
   .game-logo {
-    width: 200px;
+    width: 230px;
     transform: scale(0.5);
     opacity: 0;
     animation: popScale 0.8s ease-out 1.2s forwards;
@@ -81,9 +65,8 @@ const formattedJackpot = computed(() =>
     justify-content: space-between;
     align-items: center;
     width: 100%;
-    max-width: 100dvw;
-    padding: 1rem;
-    flex: 1;
+    max-width: 1200px;
+    padding: 2rem;
 
     @media (max-width: 1024px) {
       flex-direction: column;
@@ -91,108 +74,91 @@ const formattedJackpot = computed(() =>
     }
   }
 
-  .video {
+  .hero-left {
     flex: 1;
-    width: 350px;
-    padding-left: 5rem;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    padding-right: 2rem;
 
-    video {
-      width: 90%;
+    .info-box {
+      background-color: #28a745;
+      color: #fff;
+      padding: 2px 8px;
+      border-radius: 20px;
+      text-align: center;
+      display: inline-block;
+      box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+    }
+
+    .info {
+      font-size: small;
+      font-weight: normal;
+    }
+
+    .hero-heading {
+      font-size: 4rem;
+      font-weight: bold;
+      color: #fff;
+      text-shadow: 1px 1px 8px #fff;
+      line-height: 1;
+    }
+
+    .hero-subheading {
+      font-size: 2rem;
+      margin-bottom: 4rem;
+      text-shadow: 1px 1px 8px #fff;
+      color: #fff;
+      line-height: 1;
+    }
+
+    .claim-now-btn {
+      position: relative;
+      padding: 0.75rem 2.5rem;
+      font-size: 1.2rem;
+      font-weight: normal;
+      color: #fff;
+      background-color: var(--primary-accent); // or transparent if needed
+      border: none;
+      border-radius: 0.75rem;
+      overflow: hidden;
+      cursor: pointer;
+      z-index: 0;
+      transition: background-color 0.3s ease;
+
+      // Inner glow effect on hover
+      &:hover {
+        color: var(--primary-accent);
+        background-color: var(--tertiary-accent);
+      }
+    }
+
+    @keyframes animate-border {
+      0% {
+        background-position: 0% 50%;
+      }
+
+      100% {
+        background-position: 400% 50%;
+      }
+    }
+
+
+    .hero-video {
+      width: 100%;
+      max-width: 400px;
       border-radius: 12px;
       box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);
     }
   }
 
-  .content {
-    flex: 2;
-    max-width: 600px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 1rem;
-
-    .content-box {
-      background-color: rgba(0, 0, 0, 0.5);
-      padding: 2rem;
-      border-radius: 1rem;
-      text-align: center;
-      width: 100%;
-      box-shadow: 0 0 15px rgba(0, 0, 0, 0.4);
-
-      h1 {
-        font-size: 2rem;
-        font-weight: bold;
-        margin-bottom: 0.5rem;
-        color: #fff;
-        font-family: 'Bubblegum Sans', system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-        font-weight: 400;
-        animation: popUp 0.8s ease-out forwards;
-        text-shadow: 2px 2px 15px #000000;
-      }
-
-      .offer-sub {
-        font-size: 1rem;
-        color: var(--tertiary-accent);
-        margin-bottom: 1.5rem;
-      }
-
-      .jackpot {
-        font-size: 1.2rem;
-        margin-bottom: 2rem;
-
-        .amount {
-          font-size: 1.4rem;
-          font-weight: bold;
-          color: var(--primary-text);
-        }
-      }
-
-      .glow-button {
-        position: relative;
-        padding: 0.75rem 2rem;
-        font-size: 1.1rem;
-        font-weight: bold;
-        color: #fff;
-        background: transparent;
-        border: 2px solid var(--primary-accent);
-        border-radius: 0.5rem;
-        cursor: pointer;
-        overflow: hidden;
-        z-index: 1;
-
-        &:before {
-          content: "";
-          position: absolute;
-          top: -2px;
-          left: -2px;
-          right: -2px;
-          bottom: -2px;
-          background: linear-gradient(90deg,
-              transparent,
-              var(--primary-accent),
-              transparent);
-          animation: borderRun 2s linear infinite;
-          z-index: -1;
-        }
-
-        &:hover::before {
-          filter: brightness(1.3);
-        }
-      }
-
-      .terms {
-        font-size: 0.8rem;
-        margin-top: 0.8rem;
-        color: #ccc;
-      }
-    }
-  }
-
-  .decorations {
+  .hero-right {
     flex: 1;
     display: flex;
     flex-direction: column;
     align-items: center;
+    justify-content: center;
     gap: 1.5rem;
 
     img {
@@ -208,23 +174,6 @@ const formattedJackpot = computed(() =>
     .number {
       animation: popScale 1s 0.6s ease-out forwards;
     }
-  }
-}
-
-@keyframes borderRun {
-  0% {
-    background-position: 200% 0;
-  }
-
-  100% {
-    background-position: -200% 0;
-  }
-}
-
-@keyframes popUp {
-  to {
-    transform: translateY(15px);
-    opacity: 1;
   }
 }
 
