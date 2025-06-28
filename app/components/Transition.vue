7eef2bab6f1b5b762b80130b97897f94b0818530
Sweety_Elizabeth Jerome <jerome@fim.uni-passau.de>
2025-06-28 16:14:20 +0100

    smooth transition

---
+++ app/components/Transition.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="section-transition" />
+</template>
+
+<style scoped lang="scss">
+.section-transition {
+  width: 100%;
+  height: 80px;
+  background: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.4),
+    rgba(0, 0, 0, 0)
+  );
+  backdrop-filter: blur(10px);
+  -webkit-backdrop-filter: blur(10px);
+  transform: translateY(-20px); 
+  z-index: 2;
+  position: relative;
+  pointer-events: none;
+}
+</style>
